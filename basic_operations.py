6ac7077e88a9dc7179da84f2e383a0e48efd6c77
Phelisa Godi <phelisagodi1@gmail.com>
2025-09-17 12:23:37 +0200

    Add basic operations script

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10 #to be added to number 2
+number2 = 5 #to be added to number 1
+sum = number1 + number2
+print("The sum of", number1, "and", number2, "is", 15)
+subtraction = number1 - number2
+print ("The difference", number1, "and", number2, "is", 5)
+Multiply = number1 * number2
+print ("The multiplication", number1, "and", number2, "is" 50)
+
