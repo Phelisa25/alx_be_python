9c154e23fbbf5d1a6d995c45b003e98de378b251
Phelisa Godi <phelisagodi1@gmail.com>
2025-09-19 12:43:50 +0200

    Fixing arithmetic and update print statements

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,9 @@
 number1 = 10 #to be added to number 2
 number2 = 5 #to be added to number 1
 sum = number1 + number2
-print("The sum of", number1, "and", number2, "is", 15)
+print("The sum of", number1, "and", number2, "is", sum)
 subtraction = number1 - number2
-print ("The difference", number1, "and", number2, "is", 5)
+print ("The difference", number1, "and", number2, "is", subtraction)
 Multiply = number1 * number2
-print ("The multiplication", number1, "and", number2, "is" 50)
+print ("The multiplication", number1, "and", number2, "is", Multiply)
 
