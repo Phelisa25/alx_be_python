d53f570c1e7e70d34bfb44b2160c3a3e96739e44
Phelisa Godi <phelisagodi1@gmail.com>
2025-10-05 22:33:47 +0200

    updating temperature conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,14 @@
 def convert_to_celsius(fahrenheit):
     """Converts Fahrenheit to Celsius using the global conversion factor.
     Formula: (F - 32) * (5/9)"""
+    FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 
 def convert_to_fahrenheit(celsius):
     """Converts Celsius to Fahrenheit using the global conversion factor.
     Formula: (C * (9/5)) + 32"""
+    CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 
